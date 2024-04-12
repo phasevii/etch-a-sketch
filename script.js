8ed3c0c433f43f0c96c64f9fd071f68e43f73cec
phasevii <reidulicny@gmail.com>
2024-04-11 22:24:13 -0400

    Write extensible function to add divs to container

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const container = document.createElement("div");
 container.setAttribute("id","container");
 document.body.appendChild(container);
 
+//Will be redefined later to accept user input
+let gridSize = 16;
+
+function createGrid = for(i = 0; i <= gridSize; i++) {
+                        const gridElement = document.createElement("div");
+                        gridElement.setAttribute("class", "grid");
+                        container.appendChild(gridElement);
+                    }
