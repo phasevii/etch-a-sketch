29b6aba641b95dc33fab3d0685b114df6dd25c0a
phasevii <reidulicny@gmail.com>
2024-04-11 22:35:58 -0400

    Fix stylesheet link, function declaration

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ document.body.appendChild(container);
 //Will be redefined later to accept user input
 let gridSize = 16;
 
-function createGrid = for(i = 0; i <= gridSize; i++) {
-                        const gridElement = document.createElement("div");
-                        gridElement.setAttribute("class", "grid");
-                        container.appendChild(gridElement);
-                    }
+function createGrid() {
+    for (i = 0; i <= gridSize; i++) {
+        const gridElement = document.createElement("div");
+        gridElement.setAttribute("class", "grid");
+        container.appendChild(gridElement);
+        }
+    }
+
