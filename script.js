29f888904921626eb24ce45a47cd5c6e528e218b
phasevii <reidulicny@gmail.com>
2024-04-24 20:32:10 -0400

    Create wrapper div to hold div container

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const wrapper = document.createElement("div");
+wrapper.setAttribute("id","wrapper");
+document.body.appendChild(wrapper);
+
 const container = document.createElement("div");
 container.setAttribute("id","container");
-document.body.appendChild(container);
+document.wrapper.appendChild(container);
 
 //Will be redefined later to accept user input
 let gridSize = 16;
 
 function createGrid() {
-    for (i = 0; i <= gridSize; i++) {
+    for (i = 0; i < gridSize; i++) {
         const gridElement = document.createElement("div");
         gridElement.setAttribute("class", "grid");
         container.appendChild(gridElement);
