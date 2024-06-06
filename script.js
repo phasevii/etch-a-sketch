1f91c077f87f7bd8cd55cc38c5642ace8b67030f
reid <reid@Reids-MacBook-Pro.local>
2024-06-05 20:37:11 -0400

    Add modal dialog for grid size prompt

--- script.js
+++ script.js
@@ -33,3 +33,23 @@ const gridButton = document.createElement("button");
 gridButton.setAttribute("class", "btn");
 gridButton.textContent = "Select grid size";
 wrapper.appendChild(gridButton);
+
+const gridSizeDialog = document.createElement("dialog");
+wrapper.appendChild(gridSizeDialog);
+
+const gridSizeModalButton = document.createElement("button");
+gridSizeModalButton.setAttribute("class", "btn");
+gridSizeModalButton.textContent = "Close";
+gridSizeDialog.appendChild(gridSizeModalButton);
+
+const gridModalText = document.createElement("p");
+gridModalText.textContent = "What size is your etch-a-sketch?";
+gridSizeDialog.appendChild(gridModalText);
+
+gridButton.addEventListener("click", () => {
+    gridSizeDialog.showModal();
+})
+
+gridSizeModalButton.addEventListener("click", () => {
+    gridSizeDialog.close();
+})
